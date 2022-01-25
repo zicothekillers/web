ef65a08533266404aa3657d6e975ef9ddbe55dc4
Kiatisakt Thamjingjang <kiatisakt@gmail.com>
2022-01-25 18:34:07 +0700

    updates model

--- myblog/webblog/blogapp/models.py
+++ myblog/webblog/blogapp/models.py
@@ -1,8 +1,11 @@
+from time import timezone
 from django.db import models
 
 # Create your models here.
 class Post(models.Model) :
+    province = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200)
+    publishDate = models.CharField(max_length=80, null=True)
     body = models.TextField()
 
     def __str__(self) :
